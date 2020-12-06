930bef315e5daa2374c4b2aef9a9d3953f692989
HardSea <androidmen2000@gmail.com>
2020-12-06 19:04:36 +0200

    Homework #3

---
+++ pt3/1part.kt
@@ -0,0 +1,24 @@
+﻿//Є список, деякі елементи якого повторюються кілька разів. Знайти елемент, який повторюється найчастіше. 
+
+fun main() {
+
+    var listInt = mutableListOf<Int>()
+
+    for(i in 1..20) listInt.add((1..4).random())
+    println(listInt)
+    getOftenElement(listInt)
+    
+    println()
+    println()
+    
+    val listString = listOf<String>("a","b","c","d","a","d","c","d","a","d","c","a","d","c","c","c","d","c","d","a",)
+    
+    println(listString)
+    getOftenElement(listString)
+    
+}
+
+fun getOftenElement(list: List<*>) {
+    val oftenPair = list.groupingBy{it}.eachCount().maxByOrNull{it.value}
+    println("Element ${oftenPair?.key} is repeated ${oftenPair?.value} times")
+}
---
+++ pt3/2part.kt
@@ -0,0 +1,10 @@
+﻿//Відсортувати map<Int, String> по зростанню значення ключа. 
+
+fun main() {
+    
+    var map = mapOf<Int, String>(2 to "second", 6 to "sixth", 1 to "first", 4 to "fourth", 3 to "third", 5 to "fifth")
+    println(map)
+    map = map.toSortedMap()
+    println(map)
+    
+}
